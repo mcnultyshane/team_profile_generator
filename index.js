2ca89df127349aeaa5feec7513e794e9dc500888
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-06-25 23:22:45 -0400

    adding bulma styling to hardcoded html creation.

--- index.js
+++ index.js
@@ -13,48 +13,54 @@ const fs = require('fs');
 // WHEN I start the application
 // THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
 const promptUser = () => {
-    return inquirer.prompt([
-      {
-        type: 'input',
-        name: 'name',
-        message: "Enter employee's name",
-      },
-      {
-        type: 'input',
-        name: 'iDNumber',
-        message: "Enter employee ID,s number:",
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: "Enter employee's email address:",
-        
-    },
-    { 
-        type: 'list',
-        name: 'employeeType',
-        message: 'Enter the type of employee to register:',
-        choices: ['Manager', 'Engineer', 'Intern'],
-    },
-      {
-        type: 'input',
-        name: 'engineGithub',
-        message: "Enter the engineer's gitHub username:",
-      },
-      {
-        type: 'input',
-        name: 'schoolIntern',
-        message: "Enter the intern's school:",
-      },
-      {
-        type: 'input',
-        name: 'officeNum',
-        message: "Enter the manager's office number:",
-      },
+    return inquirer.prompt([{
+            type: 'input',
+            name: 'name',
+            message: "Enter employee's name",
+        },
+        {
+            type: 'input',
+            name: 'iDNumber',
+            message: "Enter employee ID,s number:",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Enter employee's email address:",
+        },
+        {
+            type: 'input',
+            name: 'officeNum',
+            message: "Enter the manager's office number:",
+        },
+        // WHEN I enter the team manager’s name, employee ID, email address, and office number
+        // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+        {
+            type: 'list',
+            name: 'addTeamMember',
+            message: 'Would you like to add another employee or finish building team?',
+            choices: ['Add Employee', 'Finish Team'],
+        },
+        {
+            type: 'list',
+            name: 'whichTeamMem',
+            message: 'Would you like to add an engineer or an intern?',
+            choices: ['Engineer', 'Intern'],
+        },{
+            type: 'input',
+            name: 'engineGithub',
+            message: "Enter the engineer's gitHub username:",
+        },
+        {
+            type: 'input',
+            name: 'schoolIntern',
+            message: "Enter the intern's school:",
+        },
+
     ]);
-  };
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+};
+
+
 
 // WHEN I select the engineer option
 // THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
