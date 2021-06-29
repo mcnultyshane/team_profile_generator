47accd38c76e9cbc29608d086d723a0379ca641c
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-06-29 00:37:35 -0400

    debugging inquirer code.  adding catch errors.

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+// const {
+//     generate
+// } = require('rxjs');
 
 const teamMemberArray = [];
 
@@ -13,15 +16,16 @@ const teamMemberArray = [];
 const promptMainMenu = () => {
     inquirer.prompt([
 
-        {
-            type: 'list',
-            name: 'whichTeamMem',
-            message: 'Select additional team member to add:',
-            choices: ["Engineer", "Intern", "Team Finished"],
-        }, ])
+            {
+                type: 'list',
+                name: 'whichTeamMem',
+                message: 'Select additional team member to add:',
+                choices: ["Engineer", "Intern", "Team Finished"],
+            },
+        ])
         .then(responses => {
             console.log("Your choice: " + responses)
-            switch(responses.whichTeamMem) {
+            switch (responses.whichTeamMem) {
                 case "Engineer":
                     promptEngineer();
                     break;
@@ -29,10 +33,16 @@ const promptMainMenu = () => {
                 case "Intern":
                     promptIntern();
                     break;
-                
-                case "Team Finished": 
+
+                case "Team Finished":
                     generateTeam();
-                    break;                
+                    break;
+            }
+        }).catch(error => {
+            if (error.isTtyError) {
+                return console.log(error.message)
+            } else {
+                return "unknown error involved"
             }
         })
 };
@@ -56,44 +66,49 @@ const promptIntern = () => {
                 type: 'input',
                 name: 'intIdNumber',
                 message: "Enter intern's employee ID number:",
+                default: "",
+                validate: userInput => {
+                    if (validator.isNumeric(userInput)) {
+                        return true;
+                    }
+                    return "Please enter a valid number."
+                }
             },
             {
                 type: 'input',
                 name: 'intEmail',
                 message: "Enter intern's email address:",
+                default: "",
+                validate: userInput => {
+                    if (userInput !== "") {
+                        return true
+                    }
+                    return "You have to provide an email."
+                }
             },
             {
                 type: 'input',
                 name: 'schoolIntern',
                 message: "Enter the intern's school:",
+                default: "",
+                validate: userInput => {
+                    if (userInput !== "") {
+                        return true
+                    }
+                    return "You have to provide a school."
+                }
             },
-            // WHEN I enter the team manager’s name, employee ID, email address, and office number
-            // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-            // {
-            //     type: 'confirm',
-            //     name: 'intAddTeamMember',
-            //     message: 'Would you like to add another employee?',
-            //     default: false,
-            // },
-            // {
-            //     type: 'list',
-            //     name: 'whichTeamMem',
-            //     message: 'Would you like to add an engineer or an intern?',
-            //     choices: ['Engineer', 'Intern'],
-            // },
         ])
-        .then(IntResponses => {
-            console.log("These are the Intern Inputs: " + IntResponses)
-            // if the user wants to add an employee:
-            // if (IntResponses.intAddTeamMember[0]) {
-            //     // they are asked which employee:
-            //     return promptMainMenu ()
-                    
-            // } else {
-            //     return Intresponses
-            //     // render manager page to html
-            // }
+        .then(intResponses => {
+            console.log("These are the Intern Inputs: " + intResponses)
+            const MyNewIntern = new Intern(intResponses.intName, intResponses.intIdNumber, intResponses.intEmail, intResponses.schoolIntern)
+            console.log("Intern Inputs: " + intResponses);
+            teamMemberArray.push(MyNewIntern);
+
+            promptMainMenu();
+
         })
+
 };
 
 // WHEN I select the engineer option
@@ -102,7 +117,7 @@ const promptEngineer = () => {
     return inquirer.prompt([{
                 type: 'input',
                 name: 'engName',
-                message: "Enter employee's name:",
+                message: "Enter engineer's name:",
                 default: "",
                 validate: userInput => {
                     if (userInput !== "") {
@@ -114,37 +129,33 @@ const promptEngineer = () => {
             {
                 type: 'input',
                 name: 'engIdNum',
-                message: "Enter employee ID number:",
+                message: "Enter engineer ID number:",
+                default: "",
+                validate: userInput => {
+                    if (validator.isNumeric(userInput)) {
+                        return true;
+                    }
+                    return "Please enter a valid number."
+                }
             },
             {
                 type: 'input',
                 name: 'engEmail',
-                message: "Enter employee's email address:",
+                message: "Enter engineer's email address:",
             },
             {
                 type: 'input',
                 name: 'engGithub',
                 message: "Enter the engineer's gitHub username:",
             },
-            // return back to main menu?
-            // {
-            //     type: 'confirm',
-            //     name: 'engAddTeamMember',
-            //     message: 'Would you like to add another employee?',
-            //     default: false,
-            // },
         ])
         .then(engResponses => {
-            // console.log(engResponses)
-            // // if the user wants to add an employee:
-            // if (engResponses.engAddTeamMember) {
-            //     // they are asked which employee:
-            //     return promptMainMenu ()
-                    
-            // } else {
-            //     return engResponses
-            //     // render manager page to html
-            // }
+            const MyNewEngineer = new Engineer(engResponses.engName, engResponses.engIdNum, engResponses.engEmail, engResponses.engGithub)
+            console.log("Engineer Inputs: " + engResponses)
+            teamMemberArray.push(MyNewEngineer);
+
+            promptMainMenu();
+
         })
 };
 
@@ -168,7 +179,7 @@ const promptManager = () => {
                 type: 'input',
                 name: 'iDNumber',
                 message: "Enter employee ID number:",
-                default:"",
+                default: "",
                 validate: userInput => {
                     if (validator.isNumeric(userInput)) {
                         return true;
@@ -185,7 +196,7 @@ const promptManager = () => {
                 type: 'input',
                 name: 'officeNum',
                 message: "Enter the manager's office number:",
-                default:"",
+                default: "",
                 validate: userInput => {
                     if (validator.isNumeric(userInput)) {
                         return true;
@@ -198,27 +209,34 @@ const promptManager = () => {
             // 
         ])
         .then(response => {
-            
-            const MyNewEmployee = new Manager(response.name, response.idNumber, response.email, response.officeNum);
-            teamMemberArray.push(MyNewEmployee);
+            const MyNewEmployee = new Employee(response.name, response.iDNumber, response.email);
+            const MyNewManager = new Manager(response.name, response.idNumber, response.email, response.officeNum);
+            teamMemberArray.push(MyNewManager);
             console.log("User Inputs: " + MyNewEmployee);
             console.log("New Array: " + teamMemberArray);
-           // what do you want to do next/ 
+            // what do you want to do next/ 
             promptMainMenu()
 
-            .catch(error => {
-                if (error.isTtyError) {
-                    return console.log(error.message)
-                } else {
-                    return "unknown error involved"
-                }
-            })
+                .catch(error => {
+                    if (error.isTtyError) {
+                        return console.log(error.message)
+                    } else {
+                        return "unknown error involved"
+                    }
+                })
+        }).catch(error => {
+            if (error.isTtyError) {
+                return console.log(error.message)
+            } else {
+                return "unknown error involved"
+            }
         });
-    }
+}
 // my option funciton
 // do you want to build a ___ ___ __
 // inquirer prompt
 const generateTeam = () => {
+    console.log(teamMemberArray);
 
 }
 promptManager();
