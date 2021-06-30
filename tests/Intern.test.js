389081ad9a6a91d14b22846324d69e723fc426d4
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-06-29 21:37:00 -0400

    last test working.

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,2 +1,12 @@
+const { expect } = require("@jest/globals");
 const Intern = require("../lib/Intern")
 
+test('creates an intern object', () => {
+    const employee = new Intern ('Sara', 434, 'sara@gmail.com', 'Temple University');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toStrictEqual(expect.any(String));
+    expect(employee.school).toEqual(expect.any(String));
+
+});
